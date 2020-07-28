db7bf11dc24e221d0d4d34453776b34cbdd6986c
tintin <vivien.joly@hotmail.fr>
2020-07-28 12:05:58 +0200

    poursuite de l'Ui

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -10,10 +12,33 @@ class IndexController extends AbstractController
     /**
      * @Route("/index", name="index")
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
-    }
+        // Formulaire de contact
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $message = (new \Swift_Message('Nouveau Contact'))
+                ->setFrom($contact['Email'])
+                ->setTo('votre@adresse.fr')
+                ->setBody(
+                    $this->renderView(
+                        'email/contact.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                )
+            ;
+            $mailer->send($message);
+
+
+
+        }
+
+            return $this->render('index/index.html.twig', [
+                'contactForm' => $form->createView(),
+            ]);
+        }
 }
