ca97f3cf1fb8bd7dcd7c19b0da79a3976c4fe1aa
tintin <vivien.joly@hotmail.fr>
2020-07-30 15:52:42 +0200

    poursuite de l'Ui : image ok reste a faire : - commentaire - menu

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -14,8 +15,14 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('image')
-            ->add('imageFile', vichImageType::class)
+            ->add('image', TextType::class,  [
+                'label' => "Nom de l'image",
+                'attr' => ['placeholder' => "Ajouter un nom pour l'image"]
+            ])
+            ->add('imageFile', vichImageType::class, [
+                'label' => "Ajout de l'image",
+            'attr' => ['placeholder' => "Ajouter une image"]
+        ])
             ->add('envoyer', SubmitType::class)
         ;
     }
