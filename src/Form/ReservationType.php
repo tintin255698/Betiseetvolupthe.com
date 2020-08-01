95cf8c4fc424e95b14556ee023c35b12584eeeac
tintin <vivien.joly@hotmail.fr>
2020-08-01 18:02:19 +0200

    poursuite de l'Ui : image ok reste a faire : - commentaire - menu

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,16 +23,14 @@ class ReservationType extends AbstractType
             ->add('nom', TextType::class, [ 'attr'=>['placeholder'=>'Votre nom']])
             ->add('email', EmailType::class, [ 'attr'=>['placeholder'=>'Votre email ']])
             ->add('telephone', NumberType::class, [ 'attr'=>['placeholder'=>'Votre téléphone']])
-            ->add('date', DateTimeType::class, array(
+            ->add('date', DateType::class, array(
                 'widget' => 'single_text',
                 // this is actually the default format for single_text
-                'format' => 'dd-MM-yyyy',
 
             ))
-            ->add('heure', DateTimeType::class, array(
+            ->add('heure', TimeType::class, array(
                 'widget' => 'single_text',
                 // this is actually the default format for single_text
-                'format' => 'HH-mm',
             ))
             ->add('personne', NumberType::class, [ 'attr'=>['placeholder'=>'Nombre de personne']] )
             ->add('message', TextareaType::class, [
