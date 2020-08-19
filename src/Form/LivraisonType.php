82ec9be4a9ab92258a52ea587d4c2cba28b779af
tintin <vivien.joly@hotmail.fr>
2020-08-19 07:22:25 +0200

    poursuite de l'Ui : commentaire ok reste a faire : - menu

--- src/Form/LivraisonType.php
+++ src/Form/LivraisonType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Livraison;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,11 @@ class LivraisonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('numero')
-            ->add('rue')
-            ->add('cp')
-            ->add('ville')
-            ->add('valider', SubmitType::class)
+            ->add('numero', NumberType::class, ['label'=>'Numéro de la rue', 'attr'=>['placeholder'=>'Entrer le numéro de votre rue']])
+            ->add('rue', TextType::class, ['label'=>'Rue', 'attr'=>['placeholder'=>'Votre rue']] )
+            ->add('cp', NumberType::class, ['label'=>'Code postal', 'attr'=>['placeholder'=>'Votre code postal']])
+            ->add('ville', TextType::class, ['label'=>'Ville', 'attr'=>['placeholder'=>'Votre ville']])
+            ->add('Valider', SubmitType::class)
         ;
     }
 
