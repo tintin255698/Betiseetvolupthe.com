5209925acf5484fec0ac77ca12feffe225745dca
tintin <vivien.joly@hotmail.fr>
2020-08-26 20:36:15 +0200

    poursuite de l'Ui : commentaire ok reste a faire : - inscription de la commande en bdd par webhook - visualisation de la commande restauration dans admin - faire dashboad par nouvelle techno ?

---
+++ migrations/Version20200825165548.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200825165548 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE paiement (id INT AUTO_INCREMENT NOT NULL, commande_id INT DEFAULT NULL, valider VARCHAR(255) NOT NULL, INDEX IDX_B1DC7A1E82EA2E54 (commande_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE paiement ADD CONSTRAINT FK_B1DC7A1E82EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE paiement');
+    }
+}
---
+++ src/Controller/WebhookController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Repas;
+use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebhookController extends AbstractController
+{
+    /**
+     * @Route("/webhook", name="webhook")
+     */
+    public function index(Request $request, EntityManagerInterface $em)
+    {
+
+        if ($request->isMethod('POST')) {
+            $token = $request->request->get('stripeToken');
+            \Stripe\Stripe::setApiKey("sk_test_51HEWz5LDGj5KeXGgHutzw0dSS6rfrCstf8wrV0G8Xrxwrtuc7YuNLTXXfT5KDVPHM3Xx3vv0pT04Jtj6eVjEPdj200yU5O6TaT");
+
+// This is a $20.00 charge in US Dollar.
+
+// This is a $20.00 charge in US Dollar.
+
+            try {
+            $charge = \Stripe\Charge::create(
+                array(
+                    'amount' => 2000,
+                    'currency' => 'usd',
+                    'source' => $token
+                )
+            );
+            $stripeSession = array($charge);
+            $sessId = ($stripeSession[0]['status']);
+                $repas = new Repas;
+                $repas->setType('test');
+                $repas->setPrix(2);
+                $repas->setDescription('test');
+                $repas->setProduit('test');
+                $em->persist($repas);
+                $em->flush();
+            } catch(\Stripe\Exception\CardException $e) {
+                // Since it's a decline, \Stripe\Exception\CardException will be caught
+                echo 'Status is:' . $e->getHttpStatus() . '\n';
+                echo 'Type is:' . $e->getError()->type . '\n';
+                echo 'Code is:' . $e->getError()->code . '\n';
+                // param is '' in this case
+                echo 'Param is:' . $e->getError()->param . '\n';
+                echo 'Message is:' . $e->getError()->message . '\n';
+                return $this->redirectToRoute('panier');
+            } catch (\Stripe\Exception\RateLimitException $e) {
+                return $this->redirectToRoute('panier');
+            } catch (\Stripe\Exception\InvalidRequestException $e) {
+                return $this->redirectToRoute('panier');
+            } catch (\Stripe\Exception\AuthenticationException $e) {
+                return $this->redirectToRoute('panier');
+            } catch (\Stripe\Exception\ApiConnectionException $e) {
+                return $this->redirectToRoute('panier');
+            } catch (\Stripe\Exception\ApiErrorException $e) {
+                return $this->redirectToRoute('panier');
+            } catch (Exception $e) {
+                return $this->redirectToRoute('panier');
+            }
+
+        }
+
+        return $this->render('webhook/index.html.twig', [
+            'controller_name' => 'WebhookController',
+        ]);
+    }
+}
